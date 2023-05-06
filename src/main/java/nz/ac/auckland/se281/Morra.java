ff2adb76ffec37e47a26861c9f61917fc20e52a5
Cameron Dean <camerondeannz@gmail.com>
2023-05-07 11:00:39 +1200

    Building blocks of game

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -1,14 +1,28 @@
 package nz.ac.auckland.se281;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
+  
+  private String name;
+  private List<Round> rounds;
 
-  public Morra() {}
+  public Morra() {
+    this.rounds = new ArrayList<>();
+  }
 
-  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {}
+  public void newGame(Difficulty difficulty, int pointsToWin, String[] options) {
+    this.name = options[0];
+    MessageCli.WELCOME_PLAYER.printMessage(this.name);
+  }
 
-  public void play() {}
+  public void play() {
+    MessageCli.START_ROUND.printMessage(Integer.toString(rounds.size() + 1));
+    MessageCli.ASK_INPUT.printMessage();
+  }
 
   public void showStats() {}
 }
---
+++ src/main/java/nz/ac/auckland/se281/Round.java
@@ -0,0 +1,25 @@
+package nz.ac.auckland.se281;
+
+public class Round {
+  
+  private int fingers;
+  private int sum;
+  
+  // getter methods
+  public int getFingers() {
+    return this.fingers;
+  }
+  
+  public int getSum() {
+    return this.sum;
+  }
+  
+  // setter methods
+  public void setFingers(int fingers) {
+    this.fingers = fingers;
+  }
+  
+  public void setSum(int sum) {
+    this.sum = sum;
+  }
+}
