964cc6644c0032e308ac46ce4c7b6f859a5493f6
Cameron Dean <camerondeannz@gmail.com>
2023-05-07 12:44:51 +1200

    Added user input display when choosing fingers and sum

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -6,7 +6,7 @@ import java.util.List;
 import nz.ac.auckland.se281.Main.Difficulty;
 
 public class Morra {
-  
+
   private String name;
   private List<Round> rounds;
 
@@ -22,22 +22,27 @@ public class Morra {
   public void play() {
     MessageCli.START_ROUND.printMessage(Integer.toString(rounds.size() + 1));
     MessageCli.ASK_INPUT.printMessage();
-    
+
+    // get inputs from user for this round, then display
     int[] inputs = checkValidRange();
-    System.out.println(inputs[0] + " " + inputs[1]);
     rounds.add(new Round(inputs[0], inputs[1]));
+    MessageCli.PRINT_INFO_HAND.printMessage(name, //
+        Integer.toString(rounds.get(rounds.size() - 1).getFingers()), //
+        Integer.toString(rounds.get(rounds.size() - 1).getSum()));
   }
 
-  public void showStats() {}
-  
+  public void showStats() {
+    // TODO: complete this method
+  }
+
   private int[] getInputFromUser() {
     String input = Utils.scanner.nextLine();
     String[] inputs = input.split(" ");
     inputs = checkValidInts(inputs);
-    int[] numbers = {Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1])};
+    int[] numbers = { Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1]) };
     return numbers;
   }
-  
+
   private String[] checkValidInts(String[] inputs) {
     // check if the input is two numbers that can be parsed as integers
     while (inputs.length != 2 || !Utils.isInteger(inputs[0]) || !Utils.isInteger(inputs[1])) {
@@ -46,20 +51,20 @@ public class Morra {
       String input = Utils.scanner.nextLine();
       inputs = input.split(" ");
     }
-    
+
     return inputs;
   }
-  
+
   private int[] checkValidRange() {
     int[] numbers = getInputFromUser();
-    
+
     // check if the numbers are within the valid range
     while (numbers[0] < 1 || numbers[0] > 5 || numbers[1] < 1 || numbers[1] > 10) {
       MessageCli.INVALID_INPUT.printMessage();
       MessageCli.ASK_INPUT.printMessage();
       numbers = getInputFromUser();
     }
-    
+
     return numbers;
   }
 }
--- src/main/java/nz/ac/auckland/se281/Round.java
+++ src/main/java/nz/ac/auckland/se281/Round.java
@@ -1,29 +1,29 @@
 package nz.ac.auckland.se281;
 
 public class Round {
-  
+
   private int fingers;
   private int sum;
-  
+
   public Round(int fingers, int sum) {
     this.fingers = fingers;
     this.sum = sum;
   }
-  
+
   // getter methods
   public int getFingers() {
     return this.fingers;
   }
-  
+
   public int getSum() {
     return this.sum;
   }
-  
+
   // setter methods
   public void setFingers(int fingers) {
     this.fingers = fingers;
   }
-  
+
   public void setSum(int sum) {
     this.sum = sum;
   }
