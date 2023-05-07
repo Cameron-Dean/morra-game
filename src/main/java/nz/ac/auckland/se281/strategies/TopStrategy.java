b8649a4b4458c8d1537eaf8b055eee05afea9612
Cameron Dean <camerondeannz@gmail.com>
2023-05-07 16:49:35 +1200

    Added top strategy

---
+++ src/main/java/nz/ac/auckland/se281/strategies/TopStrategy.java
@@ -0,0 +1,46 @@
+package nz.ac.auckland.se281.strategies;
+
+import java.util.List;
+import nz.ac.auckland.se281.Round;
+import nz.ac.auckland.se281.Utils;
+
+public class TopStrategy implements Strategy {
+
+  private List<Round> rounds;
+
+  public TopStrategy(List<Round> rounds) {
+    this.rounds = rounds;
+  }
+
+  @Override
+  public int[] decideFingersAndSum() {
+    int[] fingersAndSum = new int[2];
+
+    fingersAndSum[0] = Utils.getRandomNumber(1, 5);
+    fingersAndSum[1] = fingersAndSum[0] + mostCommonNumFingers();
+
+    return fingersAndSum;
+  }
+
+  private int mostCommonNumFingers() {
+    int mostCommonFingers = rounds.get(0).getFingers();
+    int mostCommonCount = 0;
+
+    for (int i = 0; i < rounds.size() - 1; i++) {
+      int count = 0;
+
+      for (int j = 0; j < rounds.size() - 1; j++) {
+        if (rounds.get(j).getFingers() == rounds.get(i).getFingers()) {
+          count++;
+        }
+
+        if (count > mostCommonCount) {
+          mostCommonFingers = rounds.get(i).getFingers();
+          mostCommonCount = count;
+        }
+      }
+    }
+
+    return mostCommonFingers;
+  }
+}
