979524aa1d1831c3d8ccea5cf7d29117649034ee
Cameron Dean <camerondeannz@gmail.com>
2023-05-07 12:19:33 +1200

    Added integer and range check for user inputs

--- src/main/java/nz/ac/auckland/se281/Morra.java
+++ src/main/java/nz/ac/auckland/se281/Morra.java
@@ -22,7 +22,44 @@ public class Morra {
   public void play() {
     MessageCli.START_ROUND.printMessage(Integer.toString(rounds.size() + 1));
     MessageCli.ASK_INPUT.printMessage();
+    
+    int[] inputs = checkValidRange();
+    System.out.println(inputs[0] + " " + inputs[1]);
+    rounds.add(new Round(inputs[0], inputs[1]));
   }
 
   public void showStats() {}
+  
+  private int[] getInputFromUser() {
+    String input = Utils.scanner.nextLine();
+    String[] inputs = input.split(" ");
+    inputs = checkValidInts(inputs);
+    int[] numbers = {Integer.parseInt(inputs[0]), Integer.parseInt(inputs[1])};
+    return numbers;
+  }
+  
+  private String[] checkValidInts(String[] inputs) {
+    // check if the input is two numbers that can be parsed as integers
+    while (inputs.length != 2 || !Utils.isInteger(inputs[0]) || !Utils.isInteger(inputs[1])) {
+      MessageCli.INVALID_INPUT.printMessage();
+      MessageCli.ASK_INPUT.printMessage();
+      String input = Utils.scanner.nextLine();
+      inputs = input.split(" ");
+    }
+    
+    return inputs;
+  }
+  
+  private int[] checkValidRange() {
+    int[] numbers = getInputFromUser();
+    
+    // check if the numbers are within the valid range
+    while (numbers[0] < 1 || numbers[0] > 5 || numbers[1] < 1 || numbers[1] > 10) {
+      MessageCli.INVALID_INPUT.printMessage();
+      MessageCli.ASK_INPUT.printMessage();
+      numbers = getInputFromUser();
+    }
+    
+    return numbers;
+  }
 }
--- src/main/java/nz/ac/auckland/se281/Round.java
+++ src/main/java/nz/ac/auckland/se281/Round.java
@@ -5,6 +5,11 @@ public class Round {
   private int fingers;
   private int sum;
   
+  public Round(int fingers, int sum) {
+    this.fingers = fingers;
+    this.sum = sum;
+  }
+  
   // getter methods
   public int getFingers() {
     return this.fingers;
